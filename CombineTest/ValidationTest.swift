3acbd1fe4cee77441c74dc10b90610875cc81e61
sasasan03 <sako.hiro62@gmail.com>
2024-07-14 16:08:18 +0900

    残をコミット

---
+++ CombineTest/ValidationTest.swift
@@ -0,0 +1,22 @@
+//
+//  ValidationTest.swift
+//  CombineTest
+//
+//  Created by sako0602 on 2023/12/24.
+//
+
+import SwiftUI
+import Combine
+
+struct ValidationTest: View {
+    @State private var name = ""
+    
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ValidationTest()
+}
